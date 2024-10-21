15c50d9d06fb77b58f102af1a189243491c4a7ed
Ravali2531 <ravalicanada3166@gmail.com>
2024-10-21 00:54:15 -0400

    Added signin signup and forgot password pages

---
+++ app/src/main/java/com/example/techlearn/Login/SignUpActivity.java
@@ -0,0 +1,112 @@
+package com.example.techlearn.Login;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.techlearn.Model.UserModel;
+import com.example.techlearn.R;
+import com.example.techlearn.databinding.ActivitySignUpBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class SignUpActivity extends AppCompatActivity {
+    ActivitySignUpBinding binding;
+    FirebaseAuth auth;
+    FirebaseDatabase database;
+    Dialog loadindDalog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+
+        loadindDalog = new Dialog(SignUpActivity.this);
+        loadindDalog.setContentView(R.layout.loading_dialog);
+
+        if(loadindDalog.getWindow() != null){
+            loadindDalog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            loadindDalog.setCancelable(false);
+        }
+
+        binding.btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = binding.edtName.getText().toString();
+                String email = binding.edtEmail.getText().toString();
+                String password = binding.edtPassword.getText().toString();
+
+                if(name.isEmpty()){
+                    binding.edtName.setError("Enter your name");
+                }
+                else if(email.isEmpty()){
+                    binding.edtName.setError("Enter your email");
+                }
+                else if(password.isEmpty()){
+                    binding.edtName.setError("Enter your password");
+                }
+                else{
+                    signup(name, email, password);
+                }
+            }
+        });
+
+        binding.alreadyAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignUpActivity.this, SignInActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+    }
+
+    private void signup(String name, String email, String password) {
+        loadindDalog.show();
+
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            String userId = task.getResult().getUser().getUid();
+
+                            UserModel userModel = new UserModel(name, email, password, "");
+
+                            database.getReference().child("user_details").child(userId)
+                                    .setValue(userModel).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if(task.isSuccessful()){
+                                                loadindDalog.dismiss();
+                                                Toast.makeText(SignUpActivity.this, "Register successfully, please verify your email id", Toast.LENGTH_SHORT).show();
+                                                onBackPressed();
+                                            }
+                                        }
+                                    });
+                        }
+                    }
+                });
+    }
+}
---
+++ app/src/main/java/com/example/techlearn/Model/UserModel.java
@@ -0,0 +1,47 @@
+package com.example.techlearn.Model;
+
+public class UserModel {
+    private String name, email, password, profile;
+
+    public UserModel() {
+    }
+
+    public UserModel(String name, String email, String password, String profile) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.profile = profile;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getProfile() {
+        return profile;
+    }
+
+    public void setProfile(String profile) {
+        this.profile = profile;
+    }
+}
