a59fa352849e32173d44f1f8fe2a61be4a44e542
Ravali2531 <ravalicanada3166@gmail.com>
2024-10-30 19:10:50 -0400

    Added Profile fragments and it's functions

--- app/src/main/java/com/example/techlearn/MainActivity.java
+++ app/src/main/java/com/example/techlearn/MainActivity.java
@@ -7,8 +7,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.example.techlearn.Fragment.HomeFragment;
+import com.example.techlearn.Fragment.ProfileFragment;
 import com.example.techlearn.databinding.ActivityMainBinding;
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +26,42 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        binding.chipNavigationBar.setItemSelected(R.id.home, true);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new HomeFragment()).commit();
+
+        binding.chipNavigationBar.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int i) {
+                Fragment selectedFragment = null;
+
+                // Navigate to the correct fragment based on selection
+                if (i == R.id.home) {
+                    binding.toolbarTitle.setText("TechLearn");
+                    selectedFragment = new HomeFragment();
+                } else if (i == R.id.profile) {
+                    binding.toolbarTitle.setText("Profile");
+                    selectedFragment = new ProfileFragment(); // Load your profile fragment here
+                }
+
+                // Load the selected fragment
+                if (selectedFragment != null) {
+                    loadFragment(selectedFragment);
+                }
+            }
+        });
+
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.container, fragment);
+        fragmentTransaction.commit();
     }
 }
